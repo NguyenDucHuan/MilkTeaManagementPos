a6944f4a9aa620111edc6b8e37e5b146d7dec848
zodicto <minhtan210703@gmail.com>
2024-07-25 01:33:47 +0700

    add setting

---
+++ MilkTeaManagementUI/SettingsWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MilkTeaManagement.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MilkTeaManagementUI
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        private EmployeeService _employeeService = new();
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            long loggedInEmpID = (long)Application.Current.Properties["LoggedInEmpID"];
+            var loginEmp = _employeeService.GetLoginEmployee(loggedInEmpID);
+            DataContext = loginEmp;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- MilkTeaManagementUI/obj/Debug/net8.0-windows/App.g.i.cs
+++ MilkTeaManagementUI/obj/Debug/net8.0-windows/App.g.i.cs
@@ -62,7 +62,7 @@ namespace MilkTeaManagementUI {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -74,7 +74,7 @@ namespace MilkTeaManagementUI {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/MilkTeaManagementUI;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MilkTeaManagementUI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -88,7 +88,7 @@ namespace MilkTeaManagementUI {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.5.0")]
         public static void Main() {
             MilkTeaManagementUI.App app = new MilkTeaManagementUI.App();
             app.InitializeComponent();
